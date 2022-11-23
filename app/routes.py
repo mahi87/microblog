feef13e9a18aec1ec0bf54740429ffa33775de1f
mahi87 <mahi87mnit@gmail.com>
2022-11-24 00:54:26 +0530

    submit blog form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
+from app.models import User, Post
 from flask_login import current_user,login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
@@ -12,10 +12,16 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET','POST'])
+@app.route('/index', methods=['GET','POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('index'))
     posts = [
         {
             'author' : { 'username': 'John'},
@@ -26,7 +32,7 @@ def index():
             'body' : 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html',title='Home', posts=posts)
+    return render_template('index.html',title='Home', posts=posts, form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
